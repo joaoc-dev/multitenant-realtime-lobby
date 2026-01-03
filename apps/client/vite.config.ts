53f0e0432b486226d55b54434078bf182325ee2d
joaoc-dev <contact@joao-carvalho.dev>
2026-01-03 21:18:35 +0000

    chore: configure shadcn

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   // Note: We use backend CORS (configured in ASP.NET Core) instead of a proxy.
   // server: {
   //   proxy: {
