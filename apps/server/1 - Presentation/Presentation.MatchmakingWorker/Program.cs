f66cb83f74949f790ec27d9cdfd51ea8a8b6a4d4
joaoc-dev <contact@joao-carvalho.dev>
2026-01-05 07:49:47 +0000

    feat: replace default logger with Serilog

--- apps/server/1 - Presentation/Presentation.MatchmakingWorker/Program.cs
+++ apps/server/1 - Presentation/Presentation.MatchmakingWorker/Program.cs
@@ -1,7 +1,36 @@
 using Presentation.MatchmakingWorker;
+using Serilog;
 
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console(
+        outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {Message:lj}{NewLine}{Exception}"
+    )
+    .Enrich.WithProperty("ServiceName", "MatchmakingWorker")
+    .Enrich.WithEnvironmentName()
+    .Enrich.WithMachineName()
+    .CreateLogger();
 
-var host = builder.Build();
-host.Run();
+try
+{
+    Log.Information("Starting Matchmaking Worker");
+
+    var builder = Host.CreateApplicationBuilder(args);
+
+    // Replace default logger with Serilog
+    builder.Services.AddSerilog();
+
+    // Register the worker
+    builder.Services.AddHostedService<Worker>();
+
+    throw new Exception("Simulated startup failure");
+    var host = builder.Build();
+    host.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Matchmaking Worker failed to start");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
